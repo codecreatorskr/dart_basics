2b0b1783cc6b2a5deb1b5d6d51b1cb354254fd9d
codecreatorskr <saurabhkumakairi501@gmail.com>
2023-08-03 00:24:56 +0530

    final commit

--- main.dart
+++ main.dart
@@ -1,10 +1,59 @@
 void main() {
 //int dataType
-  int a = 12;
-  int b = 23;
-  int c;
-  print('a: ' '$a');
-  print('b: ' '$b');
-  c = 10;
-  print('c: ' '$c');
+  // int a = 12;
+  // int b = 7;
+  // int c;
+  // // double e = 7.011;
+
+  // print('a: ' '$a');
+  // print('b: ' '$b');
+  // c = 10;
+  // print('c: ' '$c');
+  // print(a.bitLength);
+  // print(a.isEven); //For check a is even or not.
+  // print(a.isOdd); //For check b is odd or not.
+  // // print(b.isOdd); //For check b is odd or not.
+  // print(a.sign);
+  // print(a.hashCode);
+  // print(a.isFinite);
+  // print(a.isInfinite);
+  // // print(e.isNaN);
+  // print(a.isNegative);
+  // print(a.runtimeType);
+  // print(a.truncate());
+
+  //String DataType
+  // String name = 'Suraj';
+  // print('Name: ' '$name');
+  // print(name.runtimeType);
+  // print(name.indexOf('r'));
+  // print(name.allMatches('Suraj'));
+  // print(name.startsWith('S'));
+  // print(name.compareTo('Sur'));
+  // print(name.endsWith('j'));
+
+  //Double
+  // double value = 12;
+  // double val = 12.0;
+
+  // print('value: ' '$value');
+  // print('val: ' '$val');
+
+  // print(val.toInt());
+  // print(val.remainder(5));
+
+  // Boolean
+  // bool values = true;
+  // bool vals = false;
+  // print(values);
+  // print(vals);
+  // print(vals.runtimeType);
+
+  //dynamic
+  dynamic value;
+  value = 12;
+  print(value);
+
+  value = 'name';
+  print(value);
 }
