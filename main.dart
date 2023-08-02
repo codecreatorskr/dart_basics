bbb7ea66958399d7ddb2d687b295ca712a84b07a
codecreatorskr <saurabhkumakairi501@gmail.com>
2023-08-02 23:16:30 +0530

    dataType

--- main.dart
+++ main.dart
@@ -1,6 +1,24 @@
-void main() {
-  int a = 20;
+  void main() {
+    int a = 20;
 
   print(a);
   print("hello");
-}
+
+
+    // String newString = '100';
+    // double value3 = 1;
+    // int value = 10;
+    // print(value);
+    // String ab = 'herg';
+    // dynamic dValue = 'hello';
+    bool bValue = false;
+    print(bValue);
+    // print(ab.toUpperCase());
+
+    // dValue = 10;
+    // value = 5;
+    // var value2;
+
+    // print(dValue.runtimeType);
+  }
+
