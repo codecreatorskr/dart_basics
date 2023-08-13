86bfbc57805fa4dd6b455d08c530b7ff6c68e655
codecreatorskr <saurabhkumakairi501@gmail.com>
2023-08-14 02:05:56 +0530

    exception handling , APIs , Stream , Extention

---
+++ extention_Stream.dart
@@ -0,0 +1,32 @@
+//Stream
+void main() {
+  countDown().listen(
+      (event) {
+        print(event);
+      },
+      onDone: () => print("Done!!"),
+      onError: (error) {
+        print(error());
+      });
+}
+
+Stream<int> countDown() async* {
+  for (int i = 5; i > 0; i--) {
+    yield i;
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
+
+//Extention
+
+// void main() {
+//   String quote = "you can do it";
+
+//   print(quote.capialise());
+// }
+
+// extension Capatialise on String {
+//   String capialise() {
+//     return this[0].toUpperCase() + substring(1);
+//   }
+// }
